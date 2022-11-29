24b1de2456e628b0cd357a8a2cc528207a85a959
yoelchemla <74509202+yoelchemla@users.noreply.github.com>
2022-11-29 13:54:50 +0200

    update doctest

--- Ex1_py/q2.py
+++ Ex1_py/q2.py
@@ -1,42 +1,53 @@
-
 def four_neighbor_function(node: any) -> list:
     (x, y) = node
     return [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]
 
-#bfs algorithm
-def breadth_first_search(start=(-1, -1), end=(2, 1), neighbor_function=four_neighbor_function):
-        queue = []
-        prev = {}
-        queue.append(start)
-        visited = [start] # check the nodes that we visited
-        while queue:
-            curr = queue.pop(0) # v0
-            ans = neighbor_function(curr)
-            if curr == end: # if is the same node
-                return get_path(prev, start, end)
-            for i in ans:
-                if i not in visited: #and we didn't visit him
-                    visited.append(i)
-                    queue.append(i)
-                    prev[i] = curr
 
-                             #go from the final to the start, and add the vertex to the list
+# bfs algorithm
+def breadth_first_search(start=(-1, -1), end=(2, 1), neighbor_function=four_neighbor_function):
+    """
+            >>> breadth_first_search(start=(1,1),end=(2,2))
+            [(1,1), (1, 2), (2, 2)]
+            >>> breadth_first_search(start=(0,0),end=(2,2))
+            [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
+            >>> breadth_first_search(start=(0,0),end=(6,6))
+            [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6)]
+             >>> breadth_first_search(start=(-1,-1),end=(3,3))
+            [(-1, -2), (0, -2), (1, -2), (2, -2), (3, -2), (3, -1), (3, 0), (3, 1), (3, 2), (3, 3)]
+        """
+    queue = []
+    prev = {}
+    queue.append(start)
+    visited = [start]  # check the nodes that we visited
+    while queue:
+        curr = queue.pop(0)  # v0
+        ans = neighbor_function(curr)
+        if curr == end:  # if is the same node
+            return get_path(prev, start, end)
+        for i in ans:
+            if i not in visited:  # and we didn't visit him
+                visited.append(i)
+                queue.append(i)
+                prev[i] = curr
+
+                # go from the final to the start, and add the vertex to the list
+
+
 def get_path(path, start, end):
-        listOfPath = []
-        listOfPath.append(end)
-        current = path[end]
-        while current != start: # while we didn't finish
-            listOfPath.append(current)
-            current = path[current]
-        listOfPath.append(start)
-        new_list = []
-        for i in reversed(listOfPath):
-            new_list.append(i)
-        return new_list
+    list_of_path = [end]
+    current = path[end]
+    while current != start:  # while we didn't finish
+        list_of_path.append(current)
+        current = path[current]
+    list_of_path.append(start)
+    new_list = []
+    for i in reversed(list_of_path):
+        new_list.append(i)
+    return new_list
+
 
 if __name__ == '__main__':
-
-            #test case#
+    # test case#
 
     print("1. start=(1,-1) end=(-2,2)")
     print("   the res is : ", breadth_first_search(start=(1, -1), end=(-2, 2)), "\n")
