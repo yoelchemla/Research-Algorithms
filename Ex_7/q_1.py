f8bbca2e1970d2dc04e89911b2076bb6122d068b
yoelchemla <74509202+yoelchemla@users.noreply.github.com>
2022-12-13 14:26:35 +0200

    Update q_1.py
    fix q_1

--- Ex_7/q_1.py
+++ Ex_7/q_1.py
@@ -1,18 +1,22 @@
 import numpy as np
 import cvxpy as cp
+import time
 import random
 import matplotlib.pyplot as plt
 
+next_time = 0
 
-def my_timer(orig_func: callable):
-    import time
 
+def my_timer(orig_func):
     def wrapper(*args, **kwargs):
-        time_before = time.time()
+        global next_time
+        next_time = 0
+        time_prev = time.time()
         result = orig_func(*args, **kwargs)
-        time_after = time.time()
-        print(f'my_timer: {orig_func.__name__} ran in: {time_after-time_before} sec')
+        next_time = time.time() - time_prev
+        print(f'my_timer: {orig_func.__name__} run in: {next_time - time_prev} sec')
         return result
+
     return wrapper
 
 
@@ -31,13 +35,48 @@ def solve_by_numpy(A, B):
 # this function calculate the solution by cvxpy library
 # credit: https://github.com/erelsgl-at-ariel/research-5783/blob/main/07-python-numstack/code/6.cvxpy.ipynb
 @my_timer
-def cvxpy_solve(y, obj, constraints):
+def cvxpy_solve(x, obj, constraints):
     """
-    :param(1) y: variables
+    :param(1) x: variables
     :param(2) obj: sum of A@y-B matrix
     :param(3) constraints:
     :return: answer of the problem
     """
     prob = cp.Problem(obj, constraints)
     prob.solve()
-    return y.value
+    return x.value
+
+
+numpy_test = {}
+cvxpy_test = {}
+
+
+def generate_random_linear_equation(i):
+    global next_time
+    global numpy_test
+    global cvxpy_test
+    print("matrix from rank", i)
+    m = n = i
+    A = np.random.randint(-10, 10, (m, n))  # random numbers for the Matrix A in this range
+    B = np.random.randint(-100, 100, m)  # random numbers for the Vector B in this range
+    print(f"Matrix A {m}x{n} is: \n {A} \n, Matrix B is : \n {B} \n")
+    print("the solution of numpy is: ", solve_by_numpy(A, B))
+    numpy_test[m] = next_time
+    x = cp.Variable(n)
+    constraints = [A @ x == B]
+    obj = cp.Minimize(cp.sum(A @ x - B))  # calculate the sum of this linear equation
+    cvxpy_test[m] = next_time
+    print("the solution of cvxpy is: ", cvxpy_solve(x, obj, constraints))
+    print("\n")
+
+
+if __name__ == '__main__':
+    value1 = input("Please enter the minimum rank of the matrix :\n")
+    value2 = input("Please enter the maximum rank of the matrix :\n")
+
+    # make sure that the input is an integer
+    min_rank = int(value1)
+    max_rank = int(value2)
+
+    for i in range(min_rank, max_rank):
+        generate_random_linear_equation(i)
